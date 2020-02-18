cce6449e01e752fd6cd651263968bc7fcf30858d
unknown <trevorh.9173@gmail.com>
2020-02-18 13:38:55 -0600

    Almost done with redo hiking container

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,6 @@
 let enterButton = document.getElementById('button-enter');
 let inputForm = document.getElementById('city-input');
+let dateTime = document.getElementsByClassName('date-and-time');
 
 enterButton.addEventListener('click', () => {
     if (inputForm.value !== "") {
@@ -14,3 +15,23 @@ document.addEventListener('keydown', (event) => {
         window.location.href = `/${inputForm.value}`;
     }
 });
+
+var getDate = () => {
+    let date, days, month, year, monthsArray;
+
+    monthsArray= ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+
+    date = new Date();
+
+    days = date.getDate();
+    month = date.getMonth();
+    year = date.getFullYear();
+
+    date = `${monthsArray[month]} ${days}, ${year} `;
+
+    dateTime[0].innerHTML += date;
+
+}
+
+getDate();
